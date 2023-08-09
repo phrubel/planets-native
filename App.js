d33b5c761a1364fd56a1732b97f3158c421dac26
phrubel <phrubel42@gmail.com>
2023-08-10 02:24:17 +0600

    theme and color added

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { colors } from './src/theme/colors';
+import { spacing } from './src/theme/spacing';
+import { useFonts } from 'expo-font';
+import { SafeAreaView } from 'react-native-web';
+import { typography } from './src/theme/typography';
 
 export default function App() {
+  const [fontLoaded] = useFonts({
+    'Antonio-Medium': require('./assets/fonts/Antonio-Medium.ttf'),
+    'Spartan-Bold': require('./assets/fonts/Spartan-Bold.ttf'),
+    'Spartan-Regular': require('./assets/fonts/Spartan-Regular.ttf'),
+  });
+
+  if (!fontLoaded) {
+    return <Text>Font Loading..!!</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontWeight: 'bold', fontFamily: typography.primary }}>
+        Open this App My Phone Yay!!!
+      </Text>
+      <Text
+        style={{
+          fontFamily: typography.secondary,
+          fontSize: 26,
+          marginTop: spacing[1],
+        }}
+      >
+        Open this App My Phone Yay!!!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +38,9 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: colors.lightGreen,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
